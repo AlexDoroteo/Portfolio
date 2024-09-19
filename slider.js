0ba393f0b8d0dc4fca4fea9694937c7bd88fa1c3
Alexander Airi Padilla Doroteo <71139573+AlexDoroteo@users.noreply.github.com>
2024-09-18 18:16:28 -0600

    Add files via upload

---
+++ slider.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+
+    //Slider
+
+    $('.bxslider').bxSlider({
+        auto: true,
+        autoControls: true,
+        stopAutoOnClick: true,
+        pager: true,
+        slideWidth: 1900,
+        pager: false
+      });
+
+});
